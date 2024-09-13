c9cbeeeb34f574c8703bcdff7bb2281a0cb0ee44
DAnaselo <Abukaffanas5@gmail.com>
2024-09-14 00:08:46 +0300

    Clipboard Manager Added - Cleanup - HWAccelerated Mpv Script

--- Scripts/mpv.sh
+++ Scripts/mpv.sh
@@ -1,3 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
+hyprctl notify 1 6000 0 " Wait The Video Will Start Shortly."
 url="$(wl-paste)"
-flatpak run io.mpv.Mpv --fs --ytdl-format="bestvideo[height<=?1080]+bestaudio/best" $url
+flatpak run io.mpv.Mpv --fs --hwdec=auto --ytdl-format="bestvideo[height<=?1080]+bestaudio/best" $url
